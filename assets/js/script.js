c4cde46c9b4aecd40244cb26016600a7bbd3d516
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-05-22 12:35:43 +0000

    Add inital DOM load javascript for testing

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Wait for the DOM to finish loading 
+//Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function(){
+    let playbutton = document.getElementById("playGame");
+    
+    playbutton.addEventListener("click", function(){
+        let userInput = document.getElementById('user-input');
+        let userName = document.getElementById('user-name');        
+        console.log(userInput.value);
+        console.log(userName.innerText);
+    });
+    
+})
