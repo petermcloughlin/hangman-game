296ea1538e87dc22f1189a1e1bcdb5d0c5853d58
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-05-27 22:40:03 +0000

    Ament keyboardButtons event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 //Add event listener to keyboard buttons
-let keyboardButtons = document.getElementsByTagName('.keyboard button');
+let keyboardButtons = document.querySelectorAll('.keyboard button');
 for(let button of keyboardButtons){
     //console.log(button, button.innerText);
     button.addEventListener('click', clickLetter);   
