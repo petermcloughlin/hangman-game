c36ef8a04d1fac6c4a3b0e124f86531eb7a0e9ab
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-05-27 23:13:59 +0000

    Add class of keybtn to keyboard buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 //Add event listener to keyboard buttons
-let keyboardButtons = document.getElementsByTagName('button');
+let keyboardButtons = document.getElementsByClassName('keybtn');
 for(let button of keyboardButtons){
     //console.log(button, button.innerText);
     button.addEventListener('click', clickLetter);   
